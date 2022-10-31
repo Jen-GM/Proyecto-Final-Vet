72453eafd9543e50903d7133e0650298db6ffa51
Nox1do <noogux@gmail.com>
2022-10-31 04:08:16 +0000

    creando end point de token

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,11 +1,28 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+import os
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 api = Blueprint('api', __name__)
+
+#___________POST-TOKEN____________
+@api.route("/token", methods=["POST"])
+def login():
+    user = request.json.get("user", None)
+    password = request.json.get("password", None)
+    if user != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=user)
+    return jsonify(access_token=access_token)
+
 
 
 @api.route('/hello', methods=['POST', 'GET'])
