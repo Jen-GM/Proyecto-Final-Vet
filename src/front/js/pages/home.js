de445ed982c7c3aa947a32ff99148396de0d65df
Nox1do <noogux@gmail.com>
2022-11-01 01:58:04 +0000

    fix

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import AboutUs from "../component/aboutUs";
+import ContactUs from "../component/contactUs";
 import "../../styles/home.css";
 
 export const Home = () => {
@@ -8,22 +10,17 @@ export const Home = () => {
 	return (
 		<div className="text-center mt-5">
 			<h1>Hello Rigo!!</h1>
-			<h1>Hello Rigo!!</h1>
-			<h1>Hello Rigo!!</h1>
-			<h1>Hello Rigo!!</h1>
-			<h1>Hello Rigo!!</h1>
-			<h1>Hello Rigo!!</h1>
-			<h1>Hello Rigo!!</h1>
-			<h1>Hello Rigo!!</h1>
-			<h1>Hello Rigo!!</h1>
-			<h1>Hello Rigo!!</h1>
-			<p>
-				
-			</p>
 			<div className="alert alert-info">
 				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
 			</div>
-			
+			<p>
+				This boilerplate comes with lots of documentation:{" "}
+				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
+					Read documentation
+				</a>
+			</p>
+			<AboutUs/>
+			<ContactUs/>
 		</div>
 	);
 };
