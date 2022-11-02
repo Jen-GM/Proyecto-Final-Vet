f9ff4384b865f8098717a52b0d6942caf40287f2
Nox1do <noogux@gmail.com>
2022-11-02 05:00:51 +0000

    "version 2"

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -5,9 +5,8 @@ import "../../styles/home.css";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
-
   return (
-    <>
+    <> 
 	<div className="text-center mt-5">
         <h1>Hello Rigo!!</h1>
         <p>
