3ef96c59aa8406bdd790815a1f18db973a128448
kevald12 <kealable@gmail.com>
2022-11-03 01:56:16 +0000

    login terminado y cards corregidas

--- src/front/js/component/login.js
+++ src/front/js/component/login.js
@@ -1,13 +1,14 @@
-import React, {useContext, useState} from "react";
+import React, { useContext, useState, Component } from "react";
 import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
 
-export const Login = () =>{
-    const { store, actions } = useContext(Context);
-    const { email, setEmail } = useState("");
-    const { password, setPassword } = useState("");
+export const Login = () => {
+  const { store, actions } = useContext(Context);
+  const { email, setEmail } = useState("");
+  const { password, setPassword } = useState("");
+  
 
-
-const handleClick = () => {
+  const handleClick = () => {
     const opts = {
       method: "POST",
       body: JSON.stringify({
@@ -33,20 +34,31 @@ const handleClick = () => {
     <div className="text-center mt-5">
       <h1> Login </h1>
       <div>
+        <div className="d-grid gap-2 col-3 mx-auto">
         <input
           type="text"
           placeholder="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
+        </div>
+        <div className="d-grid gap-2 col-3 mx-auto">
         <input
           type="password"
           placeholder="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button onClick={handleClick}>Login</button>
+        </div>
+        <div className="d-grid gap-2 col-3 mx-auto">
+        <button className="btn btn-primary" onClick={handleClick}>Login</button>
+        </div>
       </div>
+      <div>
+      </div>
+      <a href="#" classname="btn btn-primary">
+        olvidaste tu contraseña?
+      </a>
     </div>
   );
 };
