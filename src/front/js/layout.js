0e4ca9c6040e3f8c9ac38f45c07335cda3d14c0d
kevald12 <kealable@gmail.com>
2022-11-05 17:00:27 +0000

    vista atencion medica terminada

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -5,7 +5,7 @@ import ScrollToTop from "./component/scrollToTop";
 import { Home } from "./pages/home";
 import { Usuarios } from "./pages/usuarios";
 import { Demo } from "./pages/demo";
-
+import { AtencionMedica } from "./pages/atencionMedica";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 
@@ -31,6 +31,7 @@ const Layout = () => {
                         <Route element={<Usuarios />} path="/usuarios" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Login />} path="/login" />
+                        <Route element={<AtencionMedica/>} path = "/atencion-medica"/>
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
