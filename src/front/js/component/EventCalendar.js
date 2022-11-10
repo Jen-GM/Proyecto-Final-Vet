271a06606cb4d0d80f5781b590d752daaad249bf
Gonza-5150 <estevesgonzalo@hotmail.com>
2022-11-10 03:24:24 +0000

    hasta acá

--- src/front/js/component/EventCalendar.js
+++ src/front/js/component/EventCalendar.js
@@ -3,34 +3,55 @@ import React, { Component } from "react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import { DayGridView } from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin, {
+  DateClickArg,
+  Draggable,
+} from "@fullcalendar/interaction";
+//import "@fullcalendar/core/main.css";
+import "@fullcalendar/daygrid/main.css";
+import "@fullcalendar/timegrid/main.css";
 
+const data = [
+  {
+    title: "Domar gato Ninja",
+    start: "2022-11-09T10:30:00",
+    //className: "bg-warning p-2",
+  },
+  {
+    title: "Matar gato Ninja",
+    start: "2022-11-10T10:40:00",
+  },
+  { title: "Eutanasia Caballo", start: "2022-11-09T16:30:00" },
+];
 export default class EventCalendar extends Component {
   // Aqui hacemos el fetch a la tabla agenda y lo guiardamos en un state para ponerlo en los event
   render() {
     return (
-      <FullCalendar
-        viewClassNames={"bg-dark"}
-        //eventClassNames={"bg-primary"}
-        plugins={[dayGridPlugin, timeGridPlugin]}
-        initialView="dayGridWeek"
-        slotDuration="00:00:30"
-        slotLabelInterval={{ hours: 1 }}
-        scrollTime="05:00"
-        slotMaxTime={"18:00:00"}
-        slotMinTime={"09:00:00"}
-        events={[
-          {
-            title: "Domar gato Ninja",
-            start: "2022-11-09T10:30:00",
-            className: "bg-warning p-2",
-          },
-          {
-            title: "Matar gato Ninja",
-            start: "2022-11-09T10:40:00",
-          },
-          { title: "Eutanasia Caballo", date: "2022-11-09" },
-        ]}
-      />
+      <div className="bg-dark">
+        <div className="container bg-secondary text-light rounded">
+          <FullCalendar
+            header={{
+              left: "prev,next",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay",
+            }}
+            locale={"es"}
+            timeZone={"America/Buenos_Aires"}
+            navLinks={true}
+            //dayHeaderClassNames={"bg-dark"}
+            viewClassNames={"bg-dark"}
+            eventClassNames={"shadow-lg p-2 mb-1 rounded bg-warning"}
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            initialView="dayGridWeek"
+            slotDuration="00:00:30"
+            slotLabelInterval={{ hours: 1 }}
+            //scrollTime="05:00"
+            slotMaxTime={"18:00:00"}
+            slotMinTime={"09:00:00"}
+            events={data}
+          />
+        </div>
+      </div>
     );
   }
 }
