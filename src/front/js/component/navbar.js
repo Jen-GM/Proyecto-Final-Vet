cbc0c0d39991097e421c9e7c6df9ff4fa71174d3
Jen-GM <jen.gonzalez.mu@gmail.com>
2022-11-11 19:22:20 +0000

    carousel y login

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -11,7 +11,7 @@ export const Navbar = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <nav className="navbar navbar-expand-lg bg-light" id="navigation">
+    <nav className="navbar navbar-expand-lg bg-light " id="navigation">
       <div className="container-fluid">
         <a className="navbar-brand ms-5 ps-2" href="/">
           <img
@@ -34,10 +34,10 @@ export const Navbar = () => {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="collapse navbar-collapse" id="#navbarNav">
             <ul className="navbar-nav p-5">
               <li className="nav-item border border-white rounded mx-3 border-opacity-75 shadow-lg">
-                <a className="nav-link px-4" href="#">
+                <a className="nav-link px-4" href="bgBeneficios">
                   Beneficios
                 </a>
               </li>
