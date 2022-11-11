686c27ed13620bf8cd38b31f9f0eebcf7b217c04
Jen-GM <jen.gonzalez.mu@gmail.com>
2022-11-11 20:11:43 +0000

    carousel y login

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,38 +3,31 @@ import React, { Component } from "react";
 export const Footer = () => (
   <>
     {/* Footer */}
-    <footer className="text-center text-lg-start bg-light text-muted sticky-bottom mt-3">
+    <footer className="text-center text-lg-start text-white mt-3">
       {/* Section: Social media */}
       {/* Section: Links  */}
       <section className="">
         <div className="container text-center text-md-start mt-5 pt-1">
-			
           {/* Grid row */}
           <div className="row mt-5">
             {/* Grid column */}
             <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-3">
               {/* Content */}
-              <h6 className="text-uppercase fw-bold mb-4">
-                {/* <i className="fas fa-gem me-1" /> */}
-                PetSource
-              </h6>
-              <p>place holder text</p>
+              <img
+                src="LogotipoPS.png"
+                alt="Logo"
+                width="150"
+                height="140"
+                className="d-inline-block align-text-top"
+              />
             </div>
             {/* Grid column */}
             {/* Grid column */}
             <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-2">
               {/* Links */}
               <h6 className="text-uppercase fw-bold mb-4">Servicios</h6>
-              <p>
-                <a href="#!" className="text-reset">
-                  Vacunación
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Desparasitación
-                </a>
-              </p>
+              <p className="text-reset">Instalación del software</p>
+              <p className="text-reset">Soporte del software</p>
             </div>
 
             <div className="col-md-1 col-lg-1 col-xl-3 mx-auto mb-md-0 mb-1">
@@ -58,10 +51,7 @@ export const Footer = () => (
       </section>
 
       {/* Copyright */}
-      <div
-        className="text-center p-1"
-        style={{ backgroundColor: "white" }}
-      >
+      <div className="text-center p-1" style={{ backgroundColor: "white" }}>
         {" "}
         2022 <a className="text-reset fw-bold">© Petsource</a>
       </div>
