860c039268e1072e917a5bd265a3fc7d5bef89be
Gonza-5150 <estevesgonzalo@hotmail.com>
2022-11-15 01:43:19 +0000

    maps not show

---
+++ src/front/js/component/map.js
@@ -0,0 +1,45 @@
+import React, { useContext, useState } from "react";
+import {
+  useJsApiLoader,
+  GoogleMap,
+  Marker,
+  Autocomplete,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
+
+const center = { lat: 48.8584, lng: 2.2945 };
+
+export const Map = () => {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries: ["places"],
+  });
+  const [map, setMap] = useState(/** @type google.maps.Map */ (null));
+  const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [distance, setDistance] = useState("");
+  const [duration, setDuration] = useState("");
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <GoogleMap
+        center={center}
+        zoom={15}
+        mapContainerStyle={{ width: "100%", height: "100%" }}
+        options={{
+          zoomControl: false,
+          streetViewControl: false,
+          mapTypeControl: false,
+          fullscreenControl: false,
+        }}
+        onLoad={(map) => setMap(map)}
+      >
+        <Marker position={center} />
+        {directionsResponse && (
+          <DirectionsRenderer directions={directionsResponse} />
+        )}
+      </GoogleMap>
+    </div>
+  );
+};
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./pages/home";
-import {Profile} from "./pages/profile";
+import { Profile } from "./pages/profile";
 import { Usuarios } from "./pages/usuarios";
 import { Demo } from "./pages/demo";
 import { AtencionMedica } from "./pages/atencionMedica";
@@ -13,9 +13,7 @@ import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import { Login } from "./component/login";
-
-
-
+import { Map } from "./component/map";
 
 //create your first component
 const Layout = () => {
@@ -23,26 +21,27 @@ const Layout = () => {
   // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
   const basename = process.env.BASENAME || "";
 
-    return (
-        <div>
-            <BrowserRouter basename={basename}>
-                <ScrollToTop>
-                    <Navbar />
-                    <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Profile />} path="/profile" />
-                        <Route element={<Usuarios />} path="/usuarios" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Login />} path="/login" />
-                        <Route element={<AtencionMedica/>} path = "/atencion-medica"/>
-                        <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} />
-                    </Routes>
-                    <Footer />
-                </ScrollToTop>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div>
+      <BrowserRouter basename={basename}>
+        <ScrollToTop>
+          <Navbar />
+          <Routes>
+            <Route element={<Home />} path="/" />
+            <Route element={<Map />} path="/map" />
+            <Route element={<Profile />} path="/profile" />
+            <Route element={<Usuarios />} path="/usuarios" />
+            <Route element={<Demo />} path="/demo" />
+            <Route element={<Login />} path="/login" />
+            <Route element={<AtencionMedica />} path="/atencion-medica" />
+            <Route element={<Single />} path="/single/:theid" />
+            <Route element={<h1>Not found!</h1>} />
+          </Routes>
+          <Footer />
+        </ScrollToTop>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
