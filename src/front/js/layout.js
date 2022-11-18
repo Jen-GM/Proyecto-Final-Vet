d2155c79b132e68e920fcdf69e8b09eb9174df91
Jen-GM <jen.gonzalez.mu@gmail.com>
2022-11-18 01:55:33 +0000

    correcciones a develop

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -5,15 +5,17 @@ import ScrollToTop from "./component/scrollToTop";
 import { Home } from "./pages/home";
 import {Profile} from "./pages/profile";
 import { Usuarios } from "./pages/usuarios";
-import { Demo } from "./pages/demo";
 import { AtencionMedica } from "./pages/atencionMedica";
-import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import { Login } from "./component/login";
 import { Expediente } from "./component/expediente";
+import EventCalendar from "./component/EventCalendar";
+
+
+
 
 
 
@@ -33,11 +35,11 @@ const Layout = () => {
                         <Route element={<Home />} path="/" />
                         <Route element={<Profile />} path="/profile" />
                         <Route element={<Usuarios />} path="/usuarios" />
-                        <Route element={<Demo />} path="/demo" />
                         <Route element={<Login />} path="/login" />
+                        <Route element={<EventCalendar />} path="/calendar" />                    
                         <Route element={<Expediente/>} path="/expediente"/>
                         <Route element={<AtencionMedica/>} path = "/atencion-medica"/>
-                        <Route element={<Single />} path="/single/:theid" />
+{/*                         <Route element={<Modal />} path = "/modal"/> */}
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
