3e6ba27568a1f9925f582ca8d54eaf46f74cf16c
Nox1do <noogux@gmail.com>
2022-11-20 04:54:47 +0000

    cambio

--- src/front/js/pages/datatable.js
+++ src/front/js/pages/datatable.js
@@ -1,177 +1,165 @@
 import React, { useEffect, useState } from "react";
 import MaterialTable from "material-table";
-import { forwardRef } from 'react';
+import { forwardRef } from "react";
 import axios from "axios";
-import {Modal, TextField, Button} from '@material-ui/core';
-import {makeStyles} from '@material-ui/core/styles';
-
-
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
+import { Modal, TextField, Button, Avatar, FormGroup, FormControlLabel, Checkbox  } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import AddBox from "@material-ui/icons/AddBox";
+import ArrowDownward from "@material-ui/icons/ArrowDownward";
+import Check from "@material-ui/icons/Check";
+import ChevronLeft from "@material-ui/icons/ChevronLeft";
+import ChevronRight from "@material-ui/icons/ChevronRight";
+import Clear from "@material-ui/icons/Clear";
+import DeleteOutline from "@material-ui/icons/DeleteOutline";
+import Edit from "@material-ui/icons/Edit";
+import FilterList from "@material-ui/icons/FilterList";
+import FirstPage from "@material-ui/icons/FirstPage";
+import LastPage from "@material-ui/icons/LastPage";
+import Remove from "@material-ui/icons/Remove";
+import SaveAlt from "@material-ui/icons/SaveAlt";
+import Search from "@material-ui/icons/Search";
+import ViewColumn from "@material-ui/icons/ViewColumn";
+import { SaveRounded } from "@material-ui/icons";
 
 const tableIcons = {
-    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
-  };
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => (
+    <ChevronRight {...props} ref={ref} />
+  )),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => (
+    <ChevronLeft {...props} ref={ref} />
+  )),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
+};
 
-  const columnas = [
-    {
-        title: 'Avatar',
-        field: 'id',
-             
-      
-     },
-        
- {
-        title: 'Cliente',
-        field: 'nombre',
-        headerStyle: {
-            backgroundColor: '#0099ff',
-            color: '#FFF'
-          },
-    },
-    {
-        title: 'Dirección',
-        field: 'direccion'
-        
-    },
-    {
-        title: 'Teléfono',
-        field: 'telefono'
-        
-    },
-    {
-        title: 'Mascota(s)',
-        field: 'nombre_mascota',
-        headerStyle: {
-            backgroundColor: '#0099ff',
-            color: '#FFF'
-          },
-    
-    }
-   
+const columnas = [
+  {
+    title: "ID",
+    field: "id",
+  },
+
+  {
+    title: "Cliente",
+    field: "nombre",
+  },
+  {
+    title: "Dirección",
+    field: "direccion",
+  },
+  {
+    title: "Teléfono",
+    field: "telefono",
+  },
+  {
+    title: "Mascota(s)",
+    field: "nombre_mascota",
+  },
 ];
 
 //Tabla clientes vista medica url
-const clientesUrl = 'https://3001-jengm-proyectofinalvet-jz9gcwspzua.ws-us77.gitpod.io/api/clientes';
+const clientesUrl =
+  "https://3001-jengm-proyectofinalvet-337axmpc5d9.ws-us77.gitpod.io/api/clientes";
 
 //estilos para material-table
 
 const useStyles = makeStyles((theme) => ({
-    modal: {
-      position: 'absolute',
-      width: 400,
-      backgroundColor: theme.palette.background.paper,
-      border: '2px solid #000',
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3),
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)'
-    },
-    iconos:{
-      cursor: 'pointer'
-    }, 
-    inputMaterial:{
-      width: '100%'
-    }
-  }));
-  
-  
-
+  modal: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  },
+  iconos: {
+    cursor: "pointer",
+  },
+  inputMaterial: {
+    width: "100%",
+  },
+}));
 
 export const Datatable = () => {
   const styles = useStyles();
   const [datos, setDatos] = useState([]);
   const [modalInsertar, setModalInsertar] = useState(false);
-  const [clienteInput, setClienteInput] = useState(
-     {
-        direccion: "",
-        id: "",
-        nombre: "",
-        nombre_mascota: "",
-        telefono: "",
-        user_id: ""
-      }
-    );
+  const [clienteInput, setClienteInput] = useState({
+    direccion: "",
+    nombre: "",
+    telefono: "",
+    nombre_mascota: "",
+    especie: "",
+    raza: "",
+    internamiento: true,
+    user_id: 2,
+  });
 
-    const handleChange=e=>{
-      const {name, value}=e.target;
-      setClienteInput(prevState=>({
-        ...prevState,
-        [name]: value
-      }));
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setClienteInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
 
-        
-        console.log(clienteInput);
-    }
-//___________metodo GET____________
+    console.log(clienteInput);
+  };
+  //___________metodo GET____________
   const getClientes = async () => {
     await axios.get(clientesUrl).then((res) => {
       setDatos(res.data.clientes);
-      console.log(res.data.clientes);
+      console.log(res.data);
     });
   };
-//__________metodo POST____________
-const postCliente = async()=>{
-  await axios.post(clientesUrl, clienteInput)
-  .then(res=>{
-    setDatos(data.concat(res.data.clientes));
-    abrirCerrarModalinsertar();
-  }).catch(error=>{
-    console.log(error);
-  })
-}
-
-
+  //__________metodo POST____________
+  const postCliente = async () => {
+    await axios
+      .post(clientesUrl, clienteInput)
+      .then((res) => {
+        setDatos(data.concat(res.data));
+        abrirCerrarModalinsertar();
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  };
 
   useEffect(() => {
     getClientes();
   }, []);
 
-
   const abrirCerrarModalinsertar = () => {
-    setModalInsertar(!modalInsertar)
-  }
+    setModalInsertar(!modalInsertar);
+  };
 
   //body  para insertar Clientes
   const bodyInsertar = (
     <div className={styles.modal}>
+      <Avatar
+        alt="Remy Sharp"
+        src="https://cdn-icons-png.flaticon.com/512/1995/1995549.png"
+      />
       <h3>Nuevo Cliente</h3>
       <TextField
         className={styles.inputMaterial}
-        label="Cliente"
-        name="cliente"
+        label="Nombre Completo"
+        name="nombre"
         onChange={handleChange}
-        
       />
       <br />
       <TextField
@@ -179,7 +167,6 @@ const postCliente = async()=>{
         label="Dirección"
         name="direccion"
         onChange={handleChange}
-       
       />
       <br />
       <TextField
@@ -187,68 +174,100 @@ const postCliente = async()=>{
         label="Teléfono"
         name="telefono"
         onChange={handleChange}
-        
       />
       <br />
       <TextField
         className={styles.inputMaterial}
         label="Mascota(s)"
         name="nombre_mascota"
-        // onChange={handleChange}
-        
+        onChange={handleChange}
+      />
+      <br />
+      {/* ______datos de mascota________ */}
+
+      <h3>Mascota</h3>
+      <TextField
+        className={styles.inputMaterial}
+        label="Especie"
+        name="especie"
+        onChange={handleChange}
+      />
+      <br />
+      <TextField
+        className={styles.inputMaterial}
+        label="Raza"
+        name="raza"
+        onChange={handleChange}
       />
       <br />
       <br />
+      <FormGroup>
+    <FormControlLabel control={<Checkbox defaultChecked   sx={{ '& .MuiSvgIcon-root': { fontSize: 28 } }} />} label="Internamiento" />
+   
+   
+    </FormGroup>
+
+      <br />
+      <br />
+      <br />
       <div align="right">
-        <Button color="primary" onClick={postCliente()} >
+        <Button color="primary" onClick={() => postCliente()}>
           Guardar
         </Button>
-        <Button onClick={()=>abrirCerrarModalinsertar()}>Cancelar</Button>
+        <Button onClick={() => abrirCerrarModalinsertar()}>Cancelar</Button>
       </div>
     </div>
   );
 
   return (
     <>
-      <Button  color="primary" onClick={()=>abrirCerrarModalinsertar()}>Nuevo</Button>
-      
-      <MaterialTable
-        columns={columnas}
-        data={datos}
-        icons={tableIcons}
-        title={"Lista de Clientes"}
-        actions={[
-          {
-            icon: Edit,
-            tooltip: "Editar Cliente",
-            onClick: (event, rowData) =>
-              alert("Editar  cliente: " + rowData.nombre),
-          },
-          {
-            icon: DeleteOutline,
-            tooltip: "Eliminar Cliente",
-            onClick: (event, rowData) =>
-              window.confirm("Eliminar cliente: " + rowData.nombre),
-          },
-        ]}
-        options={{
-          actionsColumnIndex: -1,
-        }}
-        localization={{
-          header: {
-            actions: "Editar",
-          },
-          
-        }}
-      />
- 
-     <Modal
-     open={modalInsertar}
-     onClose={abrirCerrarModalinsertar}
-     >{bodyInsertar}</Modal>
-        
-            
+      <div className="w-90 p-1 m-5" style={{ backgroundColor: "#eee" }}>
+        <MaterialTable
+          columns={columnas}
+          data={datos}
+          icons={tableIcons}
+          title={"Lista de Clientes"}
+          actions={[
+            {
+              icon: Edit,
+              tooltip: "Editar Cliente",
+              onClick: (event, rowData) =>
+                alert("Editar  cliente: " + rowData.nombre),
+            },
+            {
+              icon: DeleteOutline,
+              tooltip: "Eliminar Cliente",
+              onClick: (event, rowData) =>
+                window.confirm("Eliminar cliente: " + rowData.nombre),
+            },
+            {
+              icon: SaveRounded,
+              tooltip: "Añadir Cliente",
+              isFreeAction: true,
+              onClick: (event) => abrirCerrarModalinsertar(),
+            },
+          ]}
+          options={{
+            actionsColumnIndex: -1,
+            exportButton: true,
+            headerStyle: {
+              backgroundColor: "#279f7e",
+              color: "#FFF",
+              ttableLayout: "fixed",
+            },
+          }}
+          localization={{
+            header: {
+              actions: "Editar",
+              fontSize: 104,
+            },
+          }}
+        />
 
+        <Modal open={modalInsertar} onClose={abrirCerrarModalinsertar}>
+          {bodyInsertar}
+        </Modal>
+      </div>
     </>
   );
 };
