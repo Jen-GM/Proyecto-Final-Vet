12f0f9e98e504acc111924355a0e9ad33c46620a
Jen-GM <jen.gonzalez.mu@gmail.com>
2022-11-22 08:34:33 +0000

    ligeros arreglos navbar

--- src/front/js/component/login.js
+++ src/front/js/component/login.js
@@ -32,9 +32,9 @@ export const Login = () => {
   const handleClick = async () => {
     await actions.loginType(email, password).then(() =>{
       if (store.tipoUsuario === 1 && store.token){
-        navigate("/calendar");
+        navigate("/agenda");
       }else if(store.tipoUsuario === 2 && store.token){
-        navigate("/profile");
+        navigate("/cliente");
       }
     })
   };
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -18,38 +18,34 @@ import { Expediente } from "./component/expediente";
 import EventCalendar from "./pages/EventCalendar";
 import { Vacunacionydesparasitacion } from "./component/vacunacionydesparasitacion";
 
-
 //create your first component
 const Layout = () => {
   //the basename is used when your project is published in a subdirectory and not in the root of the domain
   // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
   const basename = process.env.BASENAME || "";
 
-    return (
-        <div>
-            <BrowserRouter basename={basename}>
-                <ScrollToTop>
-                    <Navbar />
-                    <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Profile />} path="/profile" />
-                        <Route element={<Usuarios />} path="/usuarios" />
-                        <Route element={<Datatable />} path="/datatable" />
-                        <Route element={<Datatable2 />} path="/datatable2" />
-                        <Route element={<Login />} path="/login" />
-                        <Route element={<EventCalendar />} path="/calendar" />                    
-                        <Route element={<VistaCliente />} path="/cliente" />                    
-                        <Route element={<Expediente/>} path="/expediente"/>
-                        <Route element={<AtencionMedica/>} path = "/atencion-medica"/>
-                        <Route element={<Vacunacionydesparasitacion/>} path="/vacunacion-desparasitacion"/>
-{/*                         <Route element={<Modal />} path = "/modal"/> */}
-                        <Route element={<h1>Not found!</h1>} />
-                    </Routes>
-                    <Footer />
-                </ScrollToTop>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div>
+      <BrowserRouter basename={basename}>
+        <ScrollToTop>
+          <Navbar />
+          <Routes>
+            <Route element={<Home />} path="/" />
+            <Route element={<Profile />} path="/editarperfil" />
+            <Route element={<Usuarios />} path="/usuarios" />
+            <Route element={<Datatable />} path="/usuarios" />
+            <Route element={<Datatable2 />} path="/mascotas" />
+            <Route element={<EventCalendar />} path="/agenda" />
+            <Route element={<VistaCliente />} path="/cliente" />
+            <Route element={<Expediente />} path="/expediente" />
+            <Route element={<AtencionMedica />} path="/atencionmedica" />
+            <Route element={<h1>Not found!</h1>} />
+          </Routes>
+          <Footer />
+        </ScrollToTop>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
